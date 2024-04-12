4135da87f3c86166c222f6177203982f4cf70fad
MINHTHUAN <thuan1809minh@gmail.com>
2024-04-12 23:40:07 +0700

    bai tap lon

--- js/aminetion.js
+++ js/aminetion.js
@@ -1,54 +1,65 @@
- function changeDisplay(clickedLi) {
-    // Lấy thông tin về hình ảnh và tên của người dùng từ phần tử li được click
-    var imgSrc = clickedLi.querySelector('img').src;
-    var userName = clickedLi.querySelector('strong').innerText;
+//  box one
+function changeDisplay(clickedLi) {
+  // Lấy thông tin về hình ảnh và tên của người dùng từ phần tử li được click
+  var imgSrc = clickedLi.querySelector("img").src;
+  var userName = clickedLi.querySelector("strong").innerText;
 
-    // Lấy phần tử có class là "name-display"
-    var nameDisplay = document.querySelector('.name-display');
-    var nameimg = document.querySelector('.name--img');
+  // Lấy phần tử có class là "name-display"
+  var nameDisplay = document.querySelector(".name-display");
+  var nameimg = document.querySelector(".name--img");
 
-    // Lấy phần tử img trong ".name-display"
-    var imgElement = nameDisplay.querySelector('img');
+  // Lấy phần tử img trong ".name-display"
+  var imgElement = nameDisplay.querySelector("img");
 
-    // Ẩn phần tử "name-display" và tất cả các phần tử con của nó
-    nameDisplay.style.display = "none";
-    nameimg.style.display = "none";
+  // Ẩn phần tử "name-display" và tất cả các phần tử con của nó
+  nameDisplay.style.display = "none";
+  nameimg.style.display = "none";
 
-    // Thay đổi nội dung của phần tử "name-display"
-    nameDisplay.innerHTML = '<img src="' + imgSrc + '" alt="user--two" />' +
-      '<strong>' + userName + '</strong>' +
-      '<span>Active now</span>';
+  // Thay đổi nội dung của phần tử "name-display"
+  nameDisplay.innerHTML =
+    '<img src="' +
+    imgSrc +
+    '" alt="user--two" />' +
+    "<strong>" +
+    userName +
+    "</strong>" +
+    "<span>Active now</span>";
 
-    // Tạo một thẻ <style> và thiết lập nội dung của nó bằng các quy tắc CSS
-    var style = document.createElement('style');
-    style.innerHTML = `
+  // Tạo một thẻ <style> và thiết lập nội dung của nó bằng các quy tắc CSS
+  var style = document.createElement("style");
+  style.innerHTML = `
       
-.name-display{
+.name--display__one{
    margin-top: -65px;
-   margin-left: 55px;
+   margin-left: 25px;
 }
 
-.name-display span {
+.name--display__one span {
   display: block;
   line-height: 16px;
-  margin-left: 65px;
-  margin-top: 23px;
+  margin-left: 60px;
+  margin-top: 15px;
 }
-.active img{
+.changer--active__one img{
   margin-top: 53px;
   position: relative;
   top:35px;
-  right:40px;
-
+  right:20px;
+  left:-20px;
 
 }
+.changer--active__one ion-icon{
+  position: relative;
+  left:73px;
+  top:57px;
+  z-index: 999;
 
-
-
+  
+}
 
     `;
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 
-    // Hiển thị lại phần tử "name-display" sau khi đã thay đổi nội dung
-    nameDisplay.style.display = "block";
-  }
+  // Hiển thị lại phần tử "name-display" sau khi đã thay đổi nội dung
+  nameDisplay.style.display = "block";
+}
